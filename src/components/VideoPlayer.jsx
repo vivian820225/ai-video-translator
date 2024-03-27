44047570e2ea09363056e862a1264485388e34a2
VC <vivian820225@gmail.com>
2024-03-27 11:10:32 +0800

    Modify: video loading

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,10 +1,43 @@
+import { useEffect, useState } from 'react'
 import ReactPlayer from 'react-player'
 
 const VideoPlayer = ({ videoURL }) => {
-  return videoURL ? (
-    <div className="w-full aspect-video">
-      <ReactPlayer width="100%" height="100%" url={videoURL} />
+  const [isReady, setIsReady] = useState(false)
+  const [videoID, setVideoID] = useState(null)
+
+  const extractYouTubeVideoID = (url) => {
+    if (url === null) return null
+    const regex =
+      /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/
+
+    const match = url.match(regex)
+
+    return match ? match[1] : null
+  }
+
+  useEffect(() => {
+    setVideoID(extractYouTubeVideoID(videoURL))
+  }, [videoURL])
+
+  if (!videoURL)
+    return (
+      <h2 className="w-full aspect-video grid place-content-center">
+        Please enter the video link.{' '}
+      </h2>
+    )
+
+  return (
+    <div className="relative w-full aspect-video">
+      {!isReady && (
+        <div className="absolute animate-pulse w-full h-full bg-slate-200 z-10"></div>
+      )}
+      <ReactPlayer
+        width="100%"
+        height="100%"
+        url={videoURL}
+        onReady={() => setIsReady(true)}
+      />
     </div>
-  ) : null
+  )
 }
 export default VideoPlayer
